8df0cfc757f6e654f64f5a2b084555b5bfd4382b
Eli Sarver <eli@elisarver.com>
2012-12-10 21:54:37 -0500

    Add TextInput, (preliminary name)

---
+++ src/main/java/org/selophane/elements/TextInput.java
@@ -0,0 +1,7 @@
+package org.selophane.elements;
+
+/**
+ * Text field functionality.
+ */
+public interface TextInput extends Element {
+}
---
+++ src/main/java/org/selophane/elements/impl/TextInputImpl.java
@@ -0,0 +1,11 @@
+package org.selophane.elements.impl;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lowmagnet
+ * Date: 12/10/12
+ * Time: 9:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TextInputImpl {
+}
