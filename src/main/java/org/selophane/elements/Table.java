56376ba0d9abea2fab057356011c279204d143b4
J Van Der Wee <joe_vdw@hotmail.co.uk>
2014-07-14 22:41:14 +0100

    Added to Table comments

--- src/main/java/org/selophane/elements/Table.java
+++ src/main/java/org/selophane/elements/Table.java
@@ -12,24 +12,20 @@ public interface Table extends Element {
     
 	/**
      * Gets the number of rows in the table
-     *
      * @return int equal to the number of rows in the table
      */
     int getRowCount();
     
     /**
      * Gets the number of columns in the table
-     *
      * @return int equal to the number of rows in the table
      */
     int getColumnCount();
     
     /**
      * Gets the WebElement of the cell at the specified index
-     * 
      * @param rowIdx The zero based index of the row
      * @param colIdx The zero based index of the column
-     * 
      * @return the WebElement of the cell at the specified index
      */
     WebElement getCellAtIndex(int rowIdx, int colIdx); 
